94aea6e52304989622ab53d78dcaa1089cca2456
David Turner <dave.c.turner@gmail.com>
2017-07-17 15:37:36 +0100

    Improvements

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,7 @@
 
 module Main where
 
+import Control.Monad
 import Control.Monad.Trans.Resource
 import Data.Monoid
 import Data.Word8
@@ -13,7 +14,7 @@ import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as BL
 import Data.Conduit
 import qualified Data.Conduit.List as DCL
-import Data.Conduit.Binary as DCB hiding (mapM_)
+import Data.Conduit.Binary as DCB hiding (mapM_, takeWhile)
 import qualified Data.Text.Encoding as T
 import qualified Data.Text as T
 import Text.Blaze.Svg
@@ -47,9 +48,9 @@ startTimeParser = hhmmssuu
   <*> APC.decimal                               <* AP.word8 _colon
   <*> APC.decimal                               <* AP.word8 _period
   <*> APC.decimal                               <* APC.endOfInput
-  where hhmmssuu hh mm ss uu = hh * 60 * 60 * 10000000
-                                  + mm * 60 * 10000000
-                                       + ss * 10000000
+  where hhmmssuu hh mm ss uu = hh * 60 * 60 * 1000000
+                                  + mm * 60 * 1000000
+                                       + ss * 1000000
                                             + uu
 
 durationParser :: AP.Parser Integer
@@ -97,11 +98,12 @@ within before after inner = do
   where toBS = T.encodeUtf8 . T.pack
 
 toSVG :: Monad m => Conduit STraceLine m B.ByteString
-toSVG = within "<?xml version=\"1.0\" encoding=\"UTF-8\"?><!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\"><svg width=\"500\" height=\"100000\" xmlns=\"http://www.w3.org/2000/svg\"><style>rect{fill:red;}</style>"
+toSVG = within "<?xml version=\"1.0\" encoding=\"UTF-8\"?><!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\"><svg width=\"1300\" height=\"8000\" xmlns=\"http://www.w3.org/2000/svg\"><style>rect{fill:red;}</style>"
                "</svg>" $ do
 
-  yield $ T.encodeUtf8 $ T.concat 
+  yield $ T.encodeUtf8 $ T.concat
     [ "<style>"
+    , "line.tick{stroke:#808080;stroke-width:0.8px}"
     , "rect{fill:#808080;}"
     , ".clock_gettime rect{fill:#000080;}"
     , ".close         rect{fill:#000000;}"
@@ -113,7 +115,35 @@ toSVG = within "<?xml version=\"1.0\" encoding=\"UTF-8\"?><!DOCTYPE svg PUBLIC \
     , ".splice        rect{fill:#80ff00;}"
     , "</style>"
     ]
-  
+
+  let vizWidth    = 1200  :: Double
+      vizHeight   = 8000  :: Double
+      usPerLine   = 50000 :: Double
+      lineSpacing = 10    :: Double
+      lineHeight  = 7     :: Double
+      oneLineCoord us = fromIntegral us / usPerLine * vizWidth
+      multiLineCoord c = let
+        line  = floor c `div` floor vizWidth
+        coord = c - vizWidth * fromIntegral line
+        in (line, coord)
+      centrePixel c = fromIntegral (floor c) + 0.5 :: Double
+
+  mapM_ yield $ BL.toChunks $ renderSvg $ do
+    let xTicks = takeWhile (<= vizWidth) $ map (centrePixel . oneLineCoord) $ iterate (+1000) 0
+    forM_ xTicks $ \tick -> line ! A.x1 (toValue tick)
+                                 ! A.x2 (toValue tick)
+                                 ! A.y1 (toValue (0 :: Double))
+                                 ! A.y2 (toValue vizHeight)
+                                 ! A.class_ "tick"
+
+    let yTicks = takeWhile (<= vizHeight) $ map (centrePixel . (* lineSpacing) . fromIntegral . fst . multiLineCoord . oneLineCoord)
+                                          $ iterate (+1000000) 0
+    forM_ yTicks $ \tick -> line ! A.x1 (toValue (0 :: Double))
+                                 ! A.x2 (toValue (vizWidth + 100))
+                                 ! A.y1 (toValue tick)
+                                 ! A.y2 (toValue tick)
+                                 ! A.class_ "tick"
+
   awaitForever $ \stl -> mapM_ yield $ BL.toChunks $ renderSvg $
     g ! A.class_ (toValue $ T.decodeUtf8 $ stlCall stl) $ do
       title (toMarkup $ T.decodeUtf8
@@ -122,10 +152,24 @@ toSVG = within "<?xml version=\"1.0\" encoding=\"UTF-8\"?><!DOCTYPE svg PUBLIC \
           <> stlStartTimeStr stl
           <> B.singleton _space
           <> stlCall stl <> stlDetails stl)
-      rect ! A.x (toValue (0::Int)) ! A.width (toValue (100::Int))
-           ! A.y      (toValue (fromIntegral (stlStartTimeUSec stl) * 0.03 :: Double))
-           ! A.height (toValue (fromIntegral (stlDurationUSec  stl) * 0.03 :: Double))
-  
+
+      let oneLineStart    = oneLineCoord $ stlStartTimeUSec stl
+          durationCoord   = oneLineCoord $ stlDurationUSec  stl
+
+
+          (startLine, startCoord) = multiLineCoord   oneLineStart
+          (endLine,   _)          = multiLineCoord $ oneLineStart + durationCoord
+
+          callLines = takeWhile (<= endLine) $ iterate (+1) startLine
+
+      forM_ callLines $ \thisLine -> let
+        offset = fromIntegral (thisLine - startLine) * vizWidth
+        clippedWidth = min durationCoord $ vizWidth - (startCoord - offset)
+        in rect ! A.x     (toValue $ startCoord - offset)
+                ! A.width (toValue clippedWidth)
+                ! A.y      (toValue $ lineSpacing * fromIntegral thisLine + 2)
+                ! A.height (toValue lineHeight)
+
 main :: IO ()
 main = do
   Config fileName <- OA.execParser optsInfo
